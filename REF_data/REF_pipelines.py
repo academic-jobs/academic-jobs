d7963948b47433375ac0ee510ee4cba3b7911e9d
Olivia Wilson <olivia@rtwilson.com>
2015-12-14 14:26:05 +0000

    Finished writing the REF data pipeline. All working

--- REF_data/REF_pipelines.py
+++ REF_data/REF_pipelines.py
@@ -15,11 +15,13 @@ class REFPipeline(object):
         self.ref_update = False
         self.get_unis_subs(unis=True, subs=True, ref_dept=True)
 
+
     def _check_type(self, ar, data_types):
         """Raises an error if ar is not of the type given"""
         if not type(ar) in data_types:
             raise TypeError("Only {} types are supported. Got {}.".format(data_types,
                                                                           type(ar)))
+
 
     def open_connection(self):
         """Opens a connection to the database and keeps it open."""
@@ -31,6 +33,7 @@ class REFPipeline(object):
                              autocommit = True)
         return connection
 
+
     def get_unis_subs(self, unis=False, subs=False, ref_dept=False):
         """Gets a dict of unique universities using self.connection
 
@@ -41,7 +44,7 @@ class REFPipeline(object):
         subs: bool (default: False)
             Whether to get unique list of main subject areas and ids.
         """
-        if unis == False and subs == False:
+        if unis == False and subs == False and ref_dept == False:
             return
         else:
             if unis:
@@ -124,15 +127,15 @@ class REFPipeline(object):
         None
         """
 
-        self._check_type(table, [str, unicode])
-        self._check_type(name, [str, unicode])
+        self._check_type(table, [str])
+        self._check_type(name, [str])
 
         # Find length of table and determine next id. This is assuming that
         # dict_of_unis and dict_of_subs is up-to-date.
         if table == 'university':
             length = len(self.dict_of_unis)
         elif table == 'ref_dept':
-            length == len(self.dict_of_ref)
+            length = len(self.dict_of_ref)
         else:
             raise ValueError("This function doesn't insert into tables "
                              "other than university or subject. Please "
@@ -144,7 +147,7 @@ class REFPipeline(object):
                 # id, col2, item_id, name
                 if table == 'university':
                     col2 = 'uni_name'
-                else table = 'ref_dept':
+                else:
                     col2 = 'ref_dept_name'
 
                 sql = "INSERT INTO %s (%s) VALUES('%s')" % (table, col2, self.connection.escape_string(name))
@@ -166,6 +169,7 @@ class REFPipeline(object):
 
         return row_id
 
+
     def process_item(self, item):
         # item is a single item from the csv.
 
@@ -177,15 +181,22 @@ class REFPipeline(object):
         else:
             uni_id = self.insert_uni_sub('university', item_dict['university'])
 
+        # Check if the item is in dict_of_ref
+        if item_dict['ref_dept_name'].lower() in self.dict_of_ref:
+            ref_id = self.dict_of_ref[item_dict['ref_dept_name'].lower()]
+        else:
+            ref_id = self.insert_uni_sub('ref_dept', item_dict['ref_dept_name'])
+
         item_dict['uni_id'] = str(uni_id)
+        item_dict['ref_dept_id'] = str(ref_id)
 
-        self.get_unis_subs(self.uni_update)
+        self.get_unis_subs(unis=self.uni_update, ref_dept=self.ref_update)
 
-        diff_items = ['university', ref_dept]
+        diff_items = ['university', 'ref_dept_name']
 
         # delete diff items from dict
         for d in diff_items:
-            del new_dict[d]
+            del item_dict[d]
 
         col = tuple(i[0] for i in item_dict.items())
         col = tuple(str(i) for i in col)
@@ -200,7 +211,7 @@ class REFPipeline(object):
 
         try:
             with self.connection.cursor() as cursor:
-                sql = "INSERT INTO jobs %s VALUES %s" % (col,val)
+                sql = "INSERT INTO ref %s VALUES %s" % (col,val)
                 cursor.execute(sql)
         except Exception:
             print("Oops. Either the connection is bad, or your sql didn't "
@@ -211,11 +222,63 @@ class REFPipeline(object):
 
         return item
 
-    def run(self):
+
+    def read_csv(self, filename):
+
+        list_of_dicts = []
+        bad_rows = 0
+
+        with open(filename, 'rb') as f:
+            # Wrap in try except for ease of troubleshooting.
+            # All cases where the length of a row isn't 6 will be ignored as will
+            # any that cause byte errors
+            for index, line in enumerate(f.readlines()):
+                try:
+                    line = line.decode('utf-8')
+                    line = line.strip()
+                    s = line.split(',')
+                    if len(s) > 8:
+                        continue
+                    else:
+                        result = {}
+                        try:
+                            result['university'] = s[0]
+                            result['ref_dept_name'] = s[1]
+                            result['staffFTE'] = s[2]
+                            result['fourstar'] = s[3]
+                            result['threestar'] = s[4]
+                            result['twostar'] = s[5]
+                            result['onestar'] = s[6]
+                            result['unclassified'] = s[7]
+
+                        except Exception:
+                            # If it got this far then there were more than 6 elements
+                            # in the data.
+                            #print("Did not load this item into the list. "
+                            #" It was row {}".format(index))
+                            bad_rows += 1
+                            continue
+                        list_of_dicts.append(result)
+                except Exception:
+                    #print('It went wrong after line {}'.format(line))
+                    bad_rows += 1
+                    continue
+        return list_of_dicts, bad_rows
+
+
+    def run(self, csvfile):
         # reads csv in line by line and then does all the other things
         # and checks against
-        
+        list_of_dicts, bad_rows = self.read_csv(csvfile)
+
+        # To get rid of headings column
+        list_of_dicts = list_of_dicts[1:]
+
+        for i in list_of_dicts:
+            item = self.process_item(i)
+            print(item)
+
 
 if __name__ == '__main__':
     ref_pipeline = REFPipeline()
-    ref_pipline.run()
+    # ref_pipline.run()
